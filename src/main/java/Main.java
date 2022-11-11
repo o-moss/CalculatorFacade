eae4d56361fd3f00096d5f336dfa8b2af7622d62
Olga Mokhova <radus888@gmail.com>
2022-11-11 21:47:46 +0300

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        BinOps bins = new BinOps();
+        System.out.println(bins.sum("0011", "11"));
+        System.out.println(bins.mult("0011", "11"));
+    }
+}
